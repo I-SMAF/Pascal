ff94cba86c5a2a2ebcbe456795cccbbcbda06cb5
SMAF <77810316+I-SMAF@users.noreply.github.com>
2021-03-01 14:20:25 +0300

    мелочи

--- L1S2.pas
+++ L1S2.pas
@@ -1,5 +1,5 @@
 ﻿uses 
-Masscpt in 'C:\inf\Masscpt.pas';
+Masscpt in 'Masscpt.pas';
 
 BEGIN ;
   var MainArr:array of integer;//динамический массив для обработки 
@@ -8,3 +8,4 @@ BEGIN ;
   Println(SumArr(MainArr));
   Println(if MaxNegEvenNum(MainArr)=0 then 'Null' else IntToStr(MaxNegEvenNum(MainArr)));//вывод наибольшего четного отрицательного элемента из массива(если онного нет на выход "Null")
 end.
+{@I_SMAF}
